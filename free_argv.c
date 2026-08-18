2277c7b13ef8959b3f7011c05898eb3b290a4ff5
Aurylias <brycevermorel@gmail.com>
2026-08-18 12:59:56 +0200

    New function to free our argv array

---
+++ free_argv.c
@@ -0,0 +1,20 @@
+#include "simple_shell.h"
+
+/**
+* free_argv - Free a NULL terminated array of strings and the array it self
+*
+* @argv: The array to free
+*/
+
+void free_argv(char **argv)
+{
+	int i;
+
+	if(!argv)
+		return;
+
+	for (i = 0; argv[i]; i++)
+		free(argv[i]);
+
+	free(argv);
+}
