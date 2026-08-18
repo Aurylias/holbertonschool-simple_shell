9676324e5c2ac380cc276d9444689e7fd79e190d
Aurylias <brycevermorel@gmail.com>
2026-08-18 14:09:09 +0200

    Add the function to find the path of the command and check if it existe

--- find_path.c
+++ find_path.c
@@ -1,10 +1,46 @@
 #include "simple_shell.h"
 
 /**
+* find_path - Looks for files in the PATH
 *
+* @filename: The name of the file to search
+* @env: The environment
 *
-*
-*
-*
+* Return: A pointer to the path, otherwise NULL
 *
 */
+
+char *find_path(char *filename, char **env)
+{
+	char *path = NULL, *token = NULL;
+	char buffer[PATH_MAX];
+	int i;
+
+	for (i = 0; env[i]; i++)
+	{
+		if (strncmp(env[i], "PATH=", 5) == 0)
+		{
+			path = strdup(env[i] + 5);
+			if (!path)
+				return (NULL);
+			break;
+		}
+	}
+
+	token = strtok(path, ":");
+	while (token)
+	{
+		strcpy(buffer, token);
+		strcat(buffer, "/");
+		strcat(buffer, filename);
+		if (access(buffer, X_OK) == 0)
+		{
+			printf("%s", buffer);
+			free(path);
+			return (strdup(buffer));
+		}
+	}
+
+	free(path);
+	return (NULL);
+}
