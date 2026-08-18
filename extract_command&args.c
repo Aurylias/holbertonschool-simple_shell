291c27904692679b7efe36f9b422d9cb18c41f2d
Aurylias <brycevermorel@gmail.com>
2026-08-18 12:58:20 +0200

    Write a whole new fonction

--- extract_command&args.c
+++ extract_command&args.c
@@ -8,45 +8,36 @@
 * Return: A poiter to the array of token or NULL on failure
 */
 
-char **extracter(char *line)
+char **extracter(char *p_userInput)
 {
+	char **argv = NULL;
 	char *token = NULL;
-	char *lineCpy = NULL;
-	char **tokens = NULL;
-	int i = 0, words = 0;
-	/*Create a copy of line*/
-	lineCpy = strdup(line);
-	if (!lineCpy)
-		return (NULL);
-	/*Create the first token*/
-	token = strtok(lineCpy, " ");
-	while (token != NULL)
-	{
-		words++;
-		token = strtok(NULL, " ");
-	}
-	/*Create an array that match the size of line*/
-	tokens = malloc(sizeof(char *) * (words + 1));
-	if (!tokens)
-		return (NULL);
-	tokens[words] = NULL;
-	/*Recreate the first token*/
-	free(lineCpy);
-	lineCpy = strdup(line);
-	if (!lineCpy)
-		return (NULL);
-	token = strtok(lineCpy, " ");
-	/*Loop through the tokens and add them in the array*/
-	while (token != NULL)
-	{
-		printf("%s\n", token);
-		tokens[i] = strdup(token);
-		if (!tokens[i])
-			return (NULL);
-		token = strtok(NULL, " ");
-		i++;
-	}
+	char delimiter[] = " \t\r\n\a";
+	int i = 0, count = 0;
+	char *copy = strdup(p_userInput);
 
-	free(lineCpy);
-	return (tokens);
+	token = strtok(copy, delimiter);
+	while (token)
+	{
+		count++;
+		token = strtok(NULL, delimiter);
+	}
+	free(copy);
+
+	if(count == 0)
+		return (NULL);
+
+	argv = malloc(sizeof(char *) * (count + 1));
+	if (!argv)
+		return (NULL);
+
+	token = strtok(p_userInput, delimiter);
+	while (token)
+	{
+		argv[i++] = strdup(token);
+		token = strtok(NULL, delimiter);
+	}
+	argv[i] = NULL;
+
+	return (argv);
 }
